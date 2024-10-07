4429892a04d026c437a9c22d319d8d4371ee09f3
amulya249 <50051855+amulya249@users.noreply.github.com>
2024-10-06 20:31:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const pdfparse = require('pdf-parse')
 
 const pdf_file = fs.readFileSync('PDF_Filenamepdf')
 const path = 'Word_Document_Filename.doc'
+// Amulya practice
 
+console.log("hello")
 pdfparse(pdf_file).then(function (data){
     fs.writeFileSync(path,data.text)
 })
